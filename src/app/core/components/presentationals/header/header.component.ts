cc993e61751b2dac3fe194ceb1c0cbdd4e6262e5
IsaquiasMarques <patisaquias2000@gmail.com>
2023-12-27 11:20:04 +0100

    header: weather data working

--- src/app/core/components/presentationals/header/header.component.ts
+++ src/app/core/components/presentationals/header/header.component.ts
@@ -1,5 +1,7 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, OnChanges, OnInit, PLATFORM_ID, SimpleChanges } from '@angular/core';
+import { COUNTRY_CODES } from '@core/constants/country-codes';
+import { CoreFacade } from '@core/facades/core.facade';
 import { BehaviorSubject } from 'rxjs';
 
 @Component({
@@ -7,21 +9,57 @@ import { BehaviorSubject } from 'rxjs';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
 
   constructor(
-    @Inject(PLATFORM_ID) private platformId: any
+    @Inject(PLATFORM_ID) private platformId: any,
+    private coreFacade: CoreFacade
   ) {}
 
   today: number = Date.now();
-  temperatureInCelsium: number = 28;
+  temperatureInCelsium$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   hours$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   minutes$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   private hourIntervalCounter: any;
+
+  locationPrimary: BehaviorSubject<string> = new BehaviorSubject<string>('');
+  locationSecondary: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  latitude: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  longitude: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   
   ngOnInit(): void {
     this.getCurrentTime();
     this.refreshClock();
+    this.getCoordinates();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+
+  getCoordinates(){
+
+    if ("geolocation" in navigator) {
+
+      navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
+        this.latitude.next(position.coords.latitude);
+        this.longitude.next(position.coords.longitude);
+
+        this.coreFacade.getGeolocationCoordinates(this.latitude.getValue(), this.longitude.getValue()).subscribe({
+          next: (incomingData: any) => {
+            this.temperatureInCelsium$.next(Math.floor(incomingData.main?.temp))
+            this.locationSecondary.next(incomingData.name);
+            this.locationPrimary.next(COUNTRY_CODES[incomingData.sys?.country] ?? 'Unknown country code');
+          }
+        });
+        
+      }, function(error) {
+        console.error("Erro ao obter a localização: " + error.message);
+      });
+
+    } else {
+      console.log("Geolocalização não suportada pelo navegador");
+    }
   }
 
   getCurrentTime(){
--- src/app/core/services/data/datacenter.service.ts
+++ src/app/core/services/data/datacenter.service.ts
@@ -7,6 +7,7 @@ import { BehaviorSubject } from "rxjs";
 
 export class AboutDataCenter{
 
+    weatherAppKey: BehaviorSubject<string> = new BehaviorSubject<string>('');
     contacts: BehaviorSubject<any> = new BehaviorSubject<any>({});
     team: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
     editorialStatus: BehaviorSubject<string> = new BehaviorSubject<string>('');
